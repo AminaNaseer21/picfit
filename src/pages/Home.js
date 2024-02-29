ddd62e783cdcf3af754fb68a0ebbbd56fe31676d
Matthew Lee <mwl36@txstate.edu>
2024-02-28 20:50:52 -0600

    Started typing code to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,18 @@
+import logo from './PicMyFit_logo.png';
+
 export default function Home() {
-    return <h1>Home</h1>
+  const centerPicture={
+    display: 'flex',
+    justifyContent: 'center', //Sets the picture frames to focus in the center.
+    alignItems: 'center', //Alligns everything to the center.
+    height: '20vh' // This makes the div take the full height of the viewport
+  };
+
+  
+    return (
+    <div>
+      <h1>Home</h1>                             
+      <img src={logo} alt="PicMyFit logo" />    
+    </div>
+  );
 }
