793a425070975fceb68ccd9b27746b284a18fbef
Matthew Lee <mwl36@txstate.edu>
2024-02-28 21:40:09 -0600

    (UPDATED) the home.js code.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import logo from './PicMyFit_logo.png';
 
 export default function Home() {
+/*Section 1: This section of the code centers the picture of the clothes from wardrobe and displays it in a way that can be similarly 
+seen in 'HomePagePic.jpg'. 
+*/
   const centerPicture={
     display: 'flex',
     justifyContent: 'center', //Sets the picture frames to focus in the center.
@@ -8,11 +11,59 @@ export default function Home() {
     height: '20vh' // This makes the div take the full height of the viewport
   };
 
+  /*Section 2: For this part, the code below will display images of the outfits pulled from the wardrobe and display it 
+  as said in section 1.
+  */
+  
+const favoriteButtons={ //This button should be displayed at the bottom left of the webpage.
+    display: 'flex',
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    height: '20vh', 
+    position: 'fixed',
+    width: '50%', 
+    bottom: '0', 
+    left: '0', 
+};
+const wardrobeButton={ // this should be displayed at the bottom middle of the webpage 
+    display: 'flex',
+    justifyContent: 'center', 
+    position: 'fixed', 
+    left: '50%', 
+    bottom: '0', 
+    transform: 'translateX(-50%)', //translate the 
+    width: 'auto', 
+    padding: '10px', 
+};
+const uploadButton={ //This button should be displayed at the bottom right of the webpage.
+    display: 'flex',
+    justifyContent: 'center', 
+    position: 'fixed', 
+    right: '0', 
+    bottom: '0', 
+    //transform: 'translateX(-100%)', 
+    width: 'auto', 
+    padding: '10px', 
+};
   
     return (
-    <div>
+    <div style = {centerPicture}>
       <h1>Home</h1>                             
-      <img src={logo} alt="PicMyFit logo" />    
+      <img src={logo} alt="PicMyFit logo" /> 
+
+      <div style={favoriteButtons}> 
+        <button type="favorites">favorites</button>
+      </div>   
+
+      <div style={wardrobeButton}>
+        <button type="Wardrobe">Wardrobe</button>
+      </div>  
+
+      <div style={uploadButton}>
+        <button type="Upload">Upload</button>
+      </div> 
+
     </div>
+    
   );
 }
