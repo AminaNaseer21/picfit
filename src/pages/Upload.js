685b1c850cc0cc2496dd39a9e4698840f0037a5f
Rogelio Atilano <25deadpool25@gmail.com>
2024-02-28 20:04:53 -0600

    possibly got it working

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import logo512 from '../img/logo512.png'; // Adjust the path as per your project structure
 
 export default function Upload() {
     const [image, setImage] = useState(null);
@@ -16,19 +17,17 @@ export default function Upload() {
 
     return (
         <div>
-            <div className="hero"></div>
-            <div className="card">
-                <h1>Image Input</h1>
-                <p>User's Name</p>
-                {image && <img src={image} alt="Uploaded" />}
-                <label htmlFor="input-file">Add Clothing Item Picture</label>
-                <input
-                    type="file"
-                    accept="image/jpeg, image/png, image/jpg, image/heic"
-                    id="input-file"
-                    onChange={handleImageChange}
-                />
-            </div>
+            <h1>Image Input</h1>
+            <p>User's Name</p>
+            <label htmlFor="input-file">Add Clothing Item Picture</label>
+            <input
+                type="file"
+                accept="image/jpeg, image/png, image/jpg, image/heic"
+                id="input-file"
+                onChange={handleImageChange}
+            />
+            {image && <img src={image} alt="Uploaded" style={{ maxWidth: '100%', maxHeight: '100%' }} />}
+            <img src={logo512} alt="Default Logo" /> {/* Using the imported image */}
         </div>
     );
 }
