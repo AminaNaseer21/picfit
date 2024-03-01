49f34a6f9b904b120123106beb27bd3f717da025
ramiroramirez02 <r_r888@txstate.edu>
2024-02-29 18:27:06 -0600

    Code appears on console that gets email,password from user

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,23 +1,70 @@
+import React, { useState } from 'react';
+
 export default function Profile() {
-    return (
-        <div>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    console.log('Login clicked');
+    console.log('Email:', email);
+    console.log('Password:', password);
+    // Add your login logic here
+  };
+
+  const handleSignup = () => {
+    console.log('Signup clicked');
+    console.log('Email:', email);
+    console.log('Password:', password);
+    // Add your signup logic here
+  };
+
+  return (
+    <div>
       <div id="login">
         <div className="header">
           <h1>Profile</h1>
         </div>
         <form>
           <div className="group">
-            <input id="txtEmail" type="email" />
+            <input
+              id="txtEmail"
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+            />
             <label>Email</label>
           </div>
           <div className="group">
-            <input id="txtPassword" type="password" />
+            <input
+              id="txtPassword"
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
             <label>Password</label>
           </div>
-          <button id="btnLogin" type="button" className="button buttonBlue">
+          <button
+            id="btnLogin"
+            type="button"
+            className="button buttonBlue"
+            onClick={handleLogin}
+          >
             Log in
           </button>
-          <button id="btnSignup" type="button" className="button buttonBlue">
+          <button
+            id="btnSignup"
+            type="button"
+            className="button buttonBlue"
+            onClick={handleSignup}
+          >
             Sign up
           </button>
         </form>
@@ -37,5 +84,5 @@ export default function Profile() {
         </form>
       </div>
     </div>
-    );
+  );
 }
