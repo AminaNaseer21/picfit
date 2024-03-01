9dba4e955f8165ab106bc86820f2cf7e442304a6
Rogelio Atilano <25deadpool25@gmail.com>
2024-02-29 20:16:02 -0600

    possibly working

--- src/pages/Wardrobe.js
+++ src/pages/Wardrobe.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import './Wardrobe.css'; // Make sure to create a corresponding CSS file
+
 export default function Wardrobe() {
-    return <h1>Wardrobe</h1>
+  return (
+    <div className="upload-container">
+      <input type="file" accept="image/*" className="upload-button" />
+    </div>
+  );
 }
