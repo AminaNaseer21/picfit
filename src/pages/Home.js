a89fde90bfcb185acd868cff457d83db9ff4fbd1
Matthew Lee <mwl36@txstate.edu>
2024-03-01 16:40:21 -0600

    enabled Wardrobe button on "Home" to go to wardrobe.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import './home.css'; // Make sure to import the CSS file
 
+import { Link } from 'react-router-dom';
 const Home = () => {
   return (
-    <div className="centerPicture">
+    <div className="header">
       <h1>Home</h1>                             
+      
 
       <div className="favoriteButtons"> 
         <button type="favorites">Favorites</button>
       </div>   
 
       <div className="wardrobeButton">
+        <Link to ="/Wardrobe">
         <button type="Wardrobe">Wardrobe</button>
+        </Link>
       </div>  
 
       <div className="uploadButton">
         <button type="Upload">Upload</button>
       </div> 
 
-    </div>
+      </div>
+
+     
   );
 };
 
