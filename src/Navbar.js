6d00ea1ba730f49f05a2e304058031162e3a7bc3
Umer Seliya <hme36@txstate.edu>
2024-03-25 17:07:53 -0500

    Added Profile Picture Icon

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,17 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import {getAuth} from "firebase/auth";
 import './Navbar.css';
 import logo from './img/PicMyFit_Logo.png';
+import profilePlaceholder from './img/profilePlaceholder.png';
 
 export default function Navbar() {
     return (
     <nav className="nav">
+    
         <Link to="/" className="PicMyFit">
             <img src={logo} alt="PicMyFit Logo" className="logo"/>
         </Link>
+
         <ul>
             <CustomLink to="wardrobe">Wardrobe</CustomLink>
             <CustomLink to="outfitter">Outfitter</CustomLink>
@@ -15,6 +19,11 @@ export default function Navbar() {
             <CustomLink to="signup">Signup</CustomLink>
             <CustomLink to="login">Login</CustomLink>
         </ul>
+
+        <div className="nav-right">
+            <img src={profilePlaceholder} alt="Profile" className="profilePlaceholder"/>
+        </div>
+
     </nav>
     )
 }
