e5483cc9f2f1686eb3815def20abed9361084d81
Umer Seliya <hme36@txstate.edu>
2024-03-26 00:55:29 -0500

    Added dropdown options to the auth dropdown menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -35,43 +35,53 @@ export default function Navbar() {
 
 
     return (
-    <nav className="nav">
-
-        
+        <nav className="nav">
     
-        <Link to="/" className="PicMyFit">
-            <img src={logo} alt="PicMyFit Logo" className="logo"/>
-        </Link>
-
-        <ul>
-            <CustomLink to="wardrobe">Wardrobe</CustomLink>
-            <CustomLink to="outfitter">Outfitter</CustomLink>
-        </ul>
-
-        <div className="nav-right">
-            {/* Toggle dropdown on click */}
-            <img src={profilePlaceholder} alt="Profile" className="profilePlaceholder" onClick={toggleDropdown}/>
-            {/* Dropdown menu */}
-            {isDropdownVisible && (
-                <div className="dropdown">
-                    <CustomLink to="profile">Profile</CustomLink>
-                    <CustomLink to="signup">Signup</CustomLink>
-                    <CustomLink to="login">Login</CustomLink>
+            <Link to="/" className="PicMyFit">
+                <img src={logo} alt="PicMyFit Logo" className="logo"/>
+            </Link>
+    
+            <ul>
+                <CustomLink to="wardrobe">Wardrobe</CustomLink>
+                <CustomLink to="outfitter">Outfitter</CustomLink>
+            </ul>
+    
+            <div className="nav-right">
+                {/* Toggle dropdown on click */}
+                <img src={profilePlaceholder} alt="Profile" className="profilePlaceholder" onClick={toggleDropdown}/>
+                {/* Dropdown menu */}
+                {isDropdownVisible && (
+                <div className={`dropdown ${isDropdownVisible ? 'dropdown-visible' : 'dropdown-hidden'}`}>
+                    {user ? (
+                        // Dropdown for authenticated users
+                        <>
+                            <CustomLink to="profile">Profile</CustomLink>
+                            <CustomLink to="settings">Settings</CustomLink>
+                            <CustomLink to="#" onClick={() => getAuth().signOut()}>Log Out</CustomLink>
+                        </>
+                    ) : (
+                        // Dropdown for unauthenticated users
+                        <>
+                            <CustomLink to="login">Log In</CustomLink>
+                            <CustomLink to="signup">Sign Up</CustomLink>
+                        </>
+                    )}
                 </div>
             )}
-        </div>
-
-    </nav>
-    )
-}
-
-function CustomLink({ to, children, ...props }) {
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true})
-
-    return (
-        <li className={isActive? "active" : ""}>
-            <Link to={to} {...props }>{children}</Link>
-        </li>
-    )
-}
+            </div>
+    
+        </nav>
+        )
+    }
+    
+    function CustomLink({ to, children, ...props }) {
+        const resolvedPath = useResolvedPath(to);
+        const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+    
+        // Render as 'div' or 'span' instead of 'li' if it affects the dropdown style
+        return (
+            <div className={isActive ? "active" : ""}>
+                <Link to={to} {...props}>{children}</Link>
+            </div>
+        );
+    }
