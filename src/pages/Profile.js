7f62060b314af4677705be8ebce5c2eb8ed8aee5
Umer Seliya <hme36@txstate.edu>
2024-03-26 14:29:11 -0500

    modified layout

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Profile.css';
 
-
 function ProfilePage() {
-    return (
-      <div className="profile-container">
-        <h1 className="profile-header">Your Profile</h1>
-        <div className="profile-picture"></div>
-        <form className="profile-form">
-          <input type="text" placeholder="Name" className="profile-input" />
-          <input type="email" placeholder="youremail@example.com" className="profile-input" />
-          <input type="password" placeholder="Password" className="profile-input" />
-          <input type="tel" placeholder="(555) 123-4567" className="profile-input" />
-        </form>
-      </div>
-    );
-  }
-  
-  export default ProfilePage;
+  return (
+   
+        <div className="profile-container">
+            <div className="profile-sidebar">
+                <div className="profile-picture"></div>
+                <h2 className="profile-name">Welcome "Name"</h2>
+            </div>
+            <div className="profile-main">
+                <h1 className="profile-header">Your Profile</h1>
+                <div className="profile-info">
+                <form className="profile-form">
+                    <input type="text" placeholder="Name" className="profile-input" />
+                    <input type="email" placeholder="youremail@example.com" className="profile-input" />
+                    <input type="password" placeholder="Password" className="profile-input" />
+                    <input type="tel" placeholder="(555) 123-4567" className="profile-input" />
+                </form>
+                {/* Add more sections like 'Projects', 'Recent', 'Most Viewed' as needed */}
+                </div>
+            </div>
+            </div>
+    
+    
+
+  );
+}
+
+export default ProfilePage;
