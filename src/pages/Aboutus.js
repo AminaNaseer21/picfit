7d62eb58a0739aa13703a34ea4439f9096ad5e04
Ramiro Ramirez <r_r888@txstate.edu>
2024-04-05 17:52:32 -0500

    Added about us

---
+++ src/pages/Aboutus.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Aboutus.css';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h1>About Us</h1>
+      <h2>Welcome to PicMyFit outfit generator web app!</h2>
+      <p>At PicMyFit, we understand the struggle of putting together the perfect outfit. Whether it's for a special occasion or just a day out, finding the right combination of clothing items can be daunting.</p>
+      <p>Our mission is to simplify this process for you. With our outfit generator, you can say goodbye to fashion dilemmas and hello to effortless style.</p>
+      <p>Our team is dedicated to providing you with a seamless and enjoyable experience. We're constantly working behind the scenes to improve our algorithms.</p>
+      <p>So why wait? Let us help you look and feel your best every day!</p>
+    </div>
+  );
+};
+
+export default AboutUs;
