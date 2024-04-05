7ee51d694e99175ce052be806c180bd8a49e5c86
Rogelio Atilano <bav74@txstate.edu>
2024-04-05 17:51:04 -0500

    fixed bug where it was interfering with other css files

--- src/pages/Outfitter.js
+++ src/pages/Outfitter.js
@@ -1,42 +1,41 @@
 // Outfitter.js
 
 import React from 'react';
-import './Outfitter.css';
+import styles from './Outfitter.module.css'; // Import CSS module
 import TempOutfitterImg from '../img/TempOutfitterImg.png'; // adjust the path as needed
-
 
 const Outfitter = () => {
   return (
-    <div className="outfitter-container">
-      <header>
+    <div className={styles.outfitterContainer}>
+      <header className={styles.header}>
         <h1>Today's Outfit</h1>
       </header>
-      <main className="main-content"> {/* Add className here */}
-        <section className="generate-outfit">
+      <main className={styles.mainContent}>
+        <section className={styles.generateOutfitSection}>
           <h2>Generate Outfit</h2>
-          <button>Generate Outfit</button>
+          <button className={styles.generateOutfitButton}>Generate Outfit</button>
         </section>
-        <section className="view-more-outfits">
+        <section className={styles.viewMoreOutfitsSection}>
           <h2>View More Outfits</h2>
         </section>
-        <section className="weather-outfits">
+        <section className={styles.weatherOutfitsSection}>
           <h2>WEATHER Outfits</h2>
           <p>Weather conditions: [insert weather conditions here]</p>
-          <img src={TempOutfitterImg} alt="Weather Icon" /> {/* Use the imported image */}
-          <div className="rate-outfit">
+          <img src={TempOutfitterImg} alt="Weather Icon" className={styles.weatherImage} />
+          <div className={styles.rateOutfitSection}>
             <h3>Rate this Outfit</h3>
-            <button>Rate this Outfit</button>
+            <button className={styles.rateOutfitButton}>Rate this Outfit</button>
           </div>
           <p>Why this Outfit?</p>
           <p>Edit Preferences</p>
         </section>
-        <section className="favorites">
+        <section className={styles.favoritesSection}>
           <h2>Favorites</h2>
-          <button>Favorite this outfit</button>
+          <button className={styles.favoriteButton}>Favorite this outfit</button>
           <p>NIRVANA</p>
         </section>
       </main>
-      <footer>
+      <footer className={styles.footer}>
         {/* Empty footer */}
       </footer>
     </div>
