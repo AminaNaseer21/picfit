8c91e3734733439bc4141fcab92fe414b794bcde
Rogelio Atilano <bav74@txstate.edu>
2024-04-05 18:00:49 -0500

    progress

--- src/pages/Outfitter.js
+++ src/pages/Outfitter.js
@@ -5,16 +5,18 @@ import TempOutfitterImg from '../img/TempOutfitterImg.png';
 const Outfitter = () => {
   return (
     <div className={styles.outfitterContainer}>
-      <div className={styles.outfitterButtonsContainer}>
-        {/* Buttons on the left */}
-        <button className={styles.outfitterButton}>Button 1</button>
-        <button className={styles.outfitterButton}>Button 2</button>
-        <button className={styles.outfitterButton}>Button 3</button>
-        <button className={styles.outfitterButton}>Button 4</button>
-        <button className={styles.outfitterButton}>Button 5</button>
-        <button className={styles.outfitterButton}>Button 6</button>
+      <div className={styles.outfitterContent}>
+        <div className={styles.outfitterButtonsContainer}>
+          {/* Buttons on the left */}
+          <button className={styles.outfitterButton}>Button 1</button>
+          <button className={styles.outfitterButton}>Button 2</button>
+          <button className={styles.outfitterButton}>Button 3</button>
+          <button className={styles.outfitterButton}>Button 4</button>
+          <button className={styles.outfitterButton}>Button 5</button>
+          <button className={styles.outfitterButton}>Button 6</button>
+        </div>
+        <img src={TempOutfitterImg} alt="Outfitter" className={styles.outfitterImage} />
       </div>
-      <img src={TempOutfitterImg} alt="Outfitter" className={styles.outfitterImage} />
       {/* Button on the right */}
       <button className={styles.outfitterButton}>Button 7</button>
     </div>
