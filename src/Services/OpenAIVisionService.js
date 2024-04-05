94d62e7764b9e470187fb6c0e394fa0d9b1ea465
Rogelio Atilano <bav74@txstate.edu>
2024-04-05 17:29:45 -0500

    Edits to get server to work

--- src/Services/OpenAIVisionService.js
+++ src/Services/OpenAIVisionService.js
@@ -2,6 +2,7 @@ import { OpenAI } from 'openai';
 
 const openai = new OpenAI({
   apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true
 });
 
 const analyzeImage = async (imageUrl, prompt) => {
