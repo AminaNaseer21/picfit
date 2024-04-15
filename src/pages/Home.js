49d52375b8f8a12d74e9f259dabb9b1207265dce
Rogelio Atilano <bav74@txstate.edu>
2024-04-15 17:39:45 -0500

    edits to new format

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,53 @@
-import React from 'react';
-import './home.css'; // Make sure to import the CSS file
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import './home.css'; // Import your existing CSS file
+import camera from "../img/camera.png";
 
 const Home = () => {
+  const [uploadPopupVisible, setUploadPopupVisible] = useState(false);
+  const [cameraHovered, setCameraHovered] = useState(false);
+  const history = useHistory();
+
+  const toggleUploadPopup = () => {
+    setUploadPopupVisible(!uploadPopupVisible);
+  };
+
+  const handleCameraHover = () => {
+    setCameraHovered(true);
+  };
+
+  const handleCameraLeave = () => {
+    setCameraHovered(false);
+  };
+
+  const handleCameraClick = () => {
+    // Redirect to the upload page
+    history.push('/upload');
+  };
+
   return (
-    <div className="Home">
-      {/* Centered dotted box */}
-      <div className="dotted-box"></div>
+    <div className="outfitterContainer">
+      {/* Enlarged rectangle */}
+      <div className="enlargedRectangle"></div>
 
       {/* Upload button */}
       <div className="upload-button">
-        <Link to="/Upload">
-          <button type="button">Upload</button>
-        </Link>
+        <button onClick={toggleUploadPopup}>Upload New Clothing Items</button>
+
+        {/* Upload popup */}
+        {uploadPopupVisible && (
+          <div className="upload-popup">
+            <button onClick={toggleUploadPopup}>Close</button>
+            <img
+              src={camera}
+              alt="Camera Icon"
+              className={cameraHovered ? "camera-icon hovered" : "camera-icon"}
+              onMouseEnter={handleCameraHover}
+              onMouseLeave={handleCameraLeave}
+              onClick={handleCameraClick}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
