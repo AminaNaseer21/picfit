d33b52bc88581477b8b623fe088693d1a7582aea
Rogelio Atilano <bav74@txstate.edu>
2024-04-15 18:04:24 -0500

    new pop up , but needs bug fixing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,28 +4,26 @@ import camera from "../img/camera.png";
 
 const Home = () => {
   const [uploadPopupVisible, setUploadPopupVisible] = useState(false);
-  const [cameraHovered, setCameraHovered] = useState(false);
+  const [cameraIconVisible, setCameraIconVisible] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setUploadPopupVisible(true);
-    }, 2000); // 2000 milliseconds = 2 seconds
+    }, 2000); // Show upload popup after 2 seconds
     return () => clearTimeout(timer);
   }, []); // Empty dependency array ensures this effect runs only once after initial render
 
-  const toggleUploadPopup = () => {
-    setUploadPopupVisible(!uploadPopupVisible);
-  };
-
-  const handleCameraHover = () => {
-    setCameraHovered(true);
-  };
-
-  const handleCameraLeave = () => {
-    setCameraHovered(false);
-  };
-
   const handleUploadClick = () => {
+    // Redirect to the upload page
+    window.location.href = '/upload';
+  };
+
+  const handleCloseButtonClick = () => {
+    setUploadPopupVisible(false);
+    setCameraIconVisible(true);
+  };
+
+  const handleCameraIconClick = () => {
     // Redirect to the upload page
     window.location.href = '/upload';
   };
@@ -38,18 +36,22 @@ const Home = () => {
       {/* Upload popup */}
       {uploadPopupVisible && (
         <div className="upload-popup">
-          <button className="close-button" onClick={toggleUploadPopup}>X</button>
+          <button className="close-button" onClick={handleCloseButtonClick}>X</button>
           <div className="upload-content">
-            <h2>Upload New Clothing Items</h2>
-            <img
-              src={camera}
-              alt="Camera Icon"
-              className={cameraHovered ? "camera-icon hovered" : "camera-icon"}
-              onMouseEnter={handleCameraHover}
-              onMouseLeave={handleCameraLeave}
-              onClick={handleUploadClick}
-            />
+            <h2 onClick={handleUploadClick}>Upload New Clothing Items</h2>
           </div>
+        </div>
+      )}
+
+      {/* Camera icon */}
+      {cameraIconVisible && (
+        <div className="camera-icon-container">
+          <img
+            src={camera}
+            alt="Camera Icon"
+            className="camera-icon"
+            onClick={handleCameraIconClick}
+          />
         </div>
       )}
     </div>
