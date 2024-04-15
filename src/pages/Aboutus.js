33d23a8947c67932dd114dca7545ce5252686ec1
Matthew Lee <mwl36@txstate.edu>
2024-04-15 18:10:35 -0500

    added a new header to the about us section.

--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -4,14 +4,21 @@ import './Aboutus.css';
 
 const AboutUs = () => {
   return (
+    <>
     <div className="about-us-container">
       <h1>About Us</h1>
       <h2>Welcome to PicMyFit our outfit generator web app!</h2>
+      <p>Established in 2024, PicMyFit is an advanced  outfit generator </p>
       <p>At PicMyFit, we understand the struggle of putting together the perfect outfit. Whether it's for a special occasion or just a day out, finding the right combination of clothing items can be daunting.</p>
+      <h2>Our Mission</h2>
       <p>Our mission is to simplify this process for you. With our outfit generator, you can say goodbye to fashion dilemmas and hello to effortless style.</p>
       <p>Our team is dedicated to providing you with a seamless and enjoyable experience. We're constantly working behind the scenes to improve our algorithms.</p>
       <p>So why wait? Let us help you look and feel your best every day!</p>
     </div>
+
+    <div className="Mission-Container">
+    </div>
+    </>
   );
 };
 
