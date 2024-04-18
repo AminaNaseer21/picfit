54980e146058883b4d2b747b347d369c5a16f075
Umer Seliya <hme36@txstate.edu>
2024-04-18 02:10:36 -0500

    updated service

--- src/Services/getWardrobeService.js
+++ src/Services/getWardrobeService.js
@@ -1,12 +1,12 @@
 // getWardrobeService.js
 
 import { getFirestore, collection, getDocs } from "firebase/firestore";
-import { firebaseApp } from "./firebaseConfig"; // Assuming firebaseConfig is in a separate file or adjust the import based on your setup
+import { firebaseApp } from "./firebase";
 
 const db = getFirestore(firebaseApp);
 
 const getAllClothingItems = async () => {
-  const clothingCollection = collection(db, "clothing"); // Adjust "clothing" to your specific collection name
+  const clothingCollection = collection(db, "wardrobe");
   const snapshot = await getDocs(clothingCollection);
   const clothingList = snapshot.docs.map(doc => doc.data());
   return clothingList;
