9655a0ce552f11385db6e13c2ee46afc01cbf8a8
Umer Seliya <hme36@txstate.edu>
2024-04-22 18:20:26 -0500

    Added trash and heart to each item

--- src/pages/Wardrobe.js
+++ src/pages/Wardrobe.js
@@ -5,10 +5,12 @@ import { useAuth } from '../Services/authentication';
 import { useNavigate } from 'react-router-dom';
 import { collection, getDocs } from 'firebase/firestore';
 import { firestore } from '../Services/firebase';
-
 import './Wardrobe.css'; // Make sure to create a corresponding CSS file
 import camera from "../img/camera.png";
 import WeatherApp from './WeatherApp';
+import icontrashcan from '../img/icon-trashcan.png';
+import iconheart from '../img/heart.png';
+import HeartButtonImage from '../img/heart.png';
 
 export default function Wardrobe() {
     const [activeCategory, setActiveCategory] = useState('');
@@ -103,8 +105,7 @@ export default function Wardrobe() {
                 </div>
                 <div className="right-side">
                     <button className="heart-button">
-                        <svg className="empty" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0H24V24H0z"></path><path d="M16.5 3C19.538 3 22 5.5 22 9c0 7-7.5 11-10 12.5C9.5 20 2 16 2 9c0-3.5 2.5-6 5.5-6C9.36 3 11 4 12 5c1-1 2.64-2 4.5-2zm-3.566 15.604c.881-.556 1.676-1.109 2.42-1.701C18.335 14.533 20 11.943 20 9c0-2.36-1.537-4-3.5-4-1.076 0-2.24.57-3.086 1.414L12 7.828l-1.414-1.414C9.74 5.57 8.576 5 7.5 5 5.56 5 4 6.656 4 9c0 2.944 1.666 5.533 4.645 7.903.745.592 1.54 1.145 2.421 1.7.299.189.595.37.934.572.339-.202.635-.383.934-.571z"></path></svg>
-                        <svg className="filled" height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0H24V24H0z" fill="none"></path><path d="M16.5 3C19.538 3 22 5.5 22 9c0 7-7.5 11-10 12.5C9.5 20 2 16 2 9c0-3.5 2.5-6 5.5-6C9.36 3 11 4 12 5c1-1 2.64-2 4.5-2z"></path></svg>
+                        <img src={HeartButtonImage} alt="Heart" />
                     </button>
                 </div>
             </div>
@@ -138,8 +139,14 @@ export default function Wardrobe() {
                 </div>
                 <div className="items">
                     {filteredImageUrls.map((item, index) => (
-                    <div key={index} className="item-image-container">
+                        <div key={index} className="item-image-container">
                         <img src={item.imageUrl} alt={`Uploaded ${index}`} className="item-image"/>
+                            <button className="heart-button">
+                                <img src={icontrashcan} alt="trash" />
+                            </button>
+                            <button className="heart-button">
+                                <img src={iconheart} alt="heart" />
+                            </button>
                         </div>
                     ))}
                     <div className="item add-new-item" onClick={handleUploadClick}>
